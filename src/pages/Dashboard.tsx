09f81db6c42f6e5b1744c05152ae1424d674f38d
carneirodee <carneirodeyse0@gmail.com>
2024-10-19 06:20:15 -0300

    List files

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,10 @@
 export default function Dashboard() {
     return (
       <>
-      <h1>Dashboard</h1>
+      <header className="">
+          <h1 className="text-green-900 text-3xl text-gray-900 mb-3">Dashboard</h1>
+          <h5 className="text-gray-400 text-base text-gray-900 ">Dashboard &gt;</h5>
+        </header>
       </>
     );
   }
