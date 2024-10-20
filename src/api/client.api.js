a20d59bf67416082832a94fd1203852b04584ce0
carneirodee <carneirodeyse0@gmail.com>
2024-10-20 10:25:34 -0300

    Integration

--- src/api/client.api.js
+++ src/api/client.api.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
-import { BACKEND_ENDPOINT } from '../config';
+import { BACKEND_ENDPOINT, NODE_ENV, BACKEND_ENDPOINT_PRODUCTION } from '../config';
 
+const endpoint = NODE_ENV === "production" ? BACKEND_ENDPOINT_PRODUCTION : BACKEND_ENDPOINT;
 export const getAll = async () => {
-    return await axios.get(`${BACKEND_ENDPOINT}api-v1/clients`);
+    return await axios.get(`${endpoint}api-v1/clients`);
 };
